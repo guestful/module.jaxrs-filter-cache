3d782d4c00e8172f02c57d8aab9d79bc8c6a03c5
mathieucarbou <mathieu.carbou@gmail.com>
2014-10-21 15:50:33 -0400

    improvements

--- src/main/java/com/guestful/jaxrs/filter/cache/CacheControlFilter.java
+++ src/main/java/com/guestful/jaxrs/filter/cache/CacheControlFilter.java
@@ -22,6 +22,7 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 
 /**
@@ -29,20 +30,16 @@ import java.io.IOException;
  * @version $Revision: 1 $
  */
 @Priority(Priorities.HEADER_DECORATOR)
-public class CacheControlFilter implements ContainerResponseFilter
-{
+public class CacheControlFilter implements ContainerResponseFilter {
     protected CacheControl cacheControl;
 
-    public CacheControlFilter(CacheControl cacheControl)
-    {
+    public CacheControlFilter(CacheControl cacheControl) {
         this.cacheControl = cacheControl;
     }
 
     @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException
-    {
-        if (responseContext.getStatus() == 200)
-        {
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        if (responseContext.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL) {
             responseContext.getHeaders().putSingle(HttpHeaders.CACHE_CONTROL, cacheControl);
         }
     }
